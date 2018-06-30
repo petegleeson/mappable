f64e527ae16b20fdd66b96b8820fbd884a0a0236
Pete Gleeson <pgleeson@atlassian.com>
2018-07-01 00:21:33 +1000

    adds mappable

--- src/__tests__/mappable.js
+++ src/__tests__/mappable.js
@@ -1,8 +1,53 @@
-const addOne = x => x + 1;
+import mappable, { map } from "../mappable";
 
-const f = 1 |> (x => x + 1);
-console.log(f);
+const obj = {
+  one: 1,
+  two: 2
+};
 
-it('should fail', () => {
-  expect(true).toBe(false);
-})
+it("should map an object", () => {
+  const res = obj |> map(i => i + 1);
+  expect(res).toEqual({ one: 2, two: 3 });
+});
+
+it("should be able to map multiple times", () => {
+  const res = obj
+    |> map(i => i + 1)
+    |> map(i => i * 2);
+  expect(res).toEqual({ one: 4, two: 6 });
+});
+
+it("should map a promise", async () => {
+  const res = Promise.resolve(1) |> map(i => i + 1);
+  expect(await res).toEqual(2);
+});
+
+it("should map an array", () => {
+  const res = [1, 2, 3] |> map(i => i + 1);
+  expect(res).toEqual([2, 3, 4]);
+});
+
+it("should map a function", () => {
+  const res = (x => x + 1) |> map(i => i + 10);
+  expect(res(1)).toEqual(12);
+});
+
+const parent = {
+  value: 1,
+  children: [{ value: 2, children: [] }, { value: 3, children: [] }]
+};
+
+it("should map custom object", () => {
+  const treeFunctor = (fn, tree) => {
+    const { value, children } = tree;
+    return {
+      value: fn(value),
+      children: children |> map(child => treeFunctor(fn, child))
+    };
+  };
+  const res = parent |> mappable(treeFunctor) |> map(i => i + 1);
+  expect(res).toEqual({
+    value: 2,
+    children: [{ value: 3, children: [] }, { value: 4, children: [] }]
+  });
+});

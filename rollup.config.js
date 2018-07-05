209b6cb90ade3e0826286bb8bc13cb0238a70189
Pete Gleeson <petergleeson.dev@gmail.com>
2018-07-05 19:06:35 +1000

    adds babel rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,21 @@
 import pkg from "./package.json";
+import babel from "rollup-plugin-babel";
+
 // rollup.config.js
 export default {
   input: "src/index.js",
   output: [
     { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" }
+    { file: pkg.module, format: "es" },
+    {
+      name: "map",
+      file: pkg.browser,
+      format: "umd"
+    }
+  ],
+  plugins: [
+    babel({
+      exclude: "node_modules/**"
+    })
   ]
 };
